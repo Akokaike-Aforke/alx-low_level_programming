7ba70ccdfe37c1be9f9b590f2cd509416f3ed607
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-08 14:16:49 -0700

    numbers commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,13 @@
 int main(void)
 {
 	int num;
-	int num2;
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		for (num2 = '0'; num2 <= '9'; num2++)
-		{
-			putchar(num);
-			putchar(num2);
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(num);
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
