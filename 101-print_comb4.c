bec9470dfb179ae10c7588ee4e42c58b581a7583
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-08 16:22:34 -0700

    3 numbers corrected

--- 101-print_comb4.c
+++ 101-print_comb4.c
@@ -16,8 +16,9 @@ for (num2 = '0'; num2 <= '9'; num2++)
 {
 for (num3 = '0'; num3 <= '9'; num3++)
 {
-if (num1 != num2 && num1 != num3 && num2 != num3 && num1 < num2 &&
-num1 < num3 && num2 < num3)
+if (num1 != num2 && num1 != num3 && num2 != num3)
+{
+if (num1 < num2 && num1 < num3 && num2 < num3)
 {
 putchar(num1);
 putchar(num2);
@@ -28,6 +29,7 @@ else
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
