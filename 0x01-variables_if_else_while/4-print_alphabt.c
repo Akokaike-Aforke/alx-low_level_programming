b9395d9285d50610f1ed621512f93b731a015f47
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-08 12:15:39 -0700

    no letter q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter < 'z')
+	{
+		if (letter != 'q' && letter != 'e' )
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
