9352bbc42e4c97a9a09a1a797ba2cf292707f5e0
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-08 17:05:16 -0700

    3 numbers corrected again

--- 101-print_comb4.c
+++ 101-print_comb4.c
@@ -16,22 +16,22 @@ int main(void)
 		{
 			for (num3 = '0'; num3 <= '9'; num3++)
 			{
-				if (num1 != num2 && num1 != num3 && num2 != num3)
-				{
-					if (num1 < num2 && num1 < num3 && num2 < num3)
-					{
-						putchar(num1);
-						putchar(num2);
-						putchar(num3);
-						if (num1 == '7' && num2 == '8' && num3 == '9')
-							break;
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
+			if (num1 != num2 && num1 != num3 && num2 != num3)
+			{
+			if (num1 < num2 && num1 < num3 && num2 < num3)
+			{
+			putchar(num1);
+			putchar(num2);
+			putchar(num3);
+			if (num1 == '7' && num2 == '8' && num3 == '9')
+			break;
+			else
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			}
+			}
 			}
 		}
 	}
