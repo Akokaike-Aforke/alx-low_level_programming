221e60c50fb0b4e22b3e5642aaf076d77e8d317e
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-11 05:07:43 -0700

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,26 +12,23 @@ int main(void)
 
 	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-	for (num2 = '0'; num2 <= '9'; num2++)
-	{
-	for (num3 = '0'; num3 <= '9'; num3++)
-	{
-	if (num1 != num2 && num1 != num3 && num2 != num3 && num1 < num2 &&
-		       	num1 < num3 && num2 < num3)
-	{
-	putchar(num1);
-	putchar(num2);
-	putchar(num3);
-	if (num1 == '7' && num2 == '8' && num3 == '9')
-	break;
-	else
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	}
-	}
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			for (num3 = '0'; num3 <= '9'; num3++)
+			{
+				if (num1 != num2 && num1 != num3 && num2 != num3 && num1 < num2 &&
+						num1 < num3 && num2 < num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (num1 == '7' && num2 == '8' && num3 == '9')
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
