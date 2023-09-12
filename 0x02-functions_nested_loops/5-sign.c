729fe278d5a00ddab2b4ebb3a8d4976a343db126
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 11:08:19 -0700

    positive

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_sign - check for positive or negative number
+ *@n: the character to be checked
+ *
+ * Return: 1 if n is positive, 0 if n is 0, return -1 if n is negative
+ */
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-1');
+	return (-1);
+}
