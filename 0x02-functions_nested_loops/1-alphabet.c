c03289dfb954817d99edcbed253a607854d0635e
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 09:42:27 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "main.h>"
 /**
- * void print_alphabet(void) - print alphabets
+ * print_alphabet(void) - print alphabets
  *
+ * Return 0
  */
 void print_alphabet(void)
 {
