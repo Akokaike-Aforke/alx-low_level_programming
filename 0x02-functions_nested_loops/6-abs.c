130f607eb34e9d8b913a3c5dbf721f09826f660d
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 13:14:50 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
- *print_sign - check for positive or negative number
- *@n: the character to be checked
- *
- * Return: 1 if n is positive, 0 if n is 0, return -1 if n is negative
+ *print_abs - return the absolute value
  */
-int print_sign(int n)
+int _abs(int)
 {
-	if (n > 1)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	_putchar('-1');
-	return (-1);
+	return abs(int)
 }
