d7cf7f796dfb23a3eabe771275428e5b106af2c7
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 10:27:24 -0700

    check lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int _islower(int c) - check for lower case
+ *
+ * Return - 0 for lower 1 for upper
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && <= 'z')
+		return 0;
+	return 1;
+}
