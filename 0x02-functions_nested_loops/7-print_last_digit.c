c262b831060590138abcab0fe412d1f7731556b7
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 14:46:13 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,9 @@
  */
 int print_last_digit(int num)
 {
-	_putchar(num % 10);
-	return (num % 10);
+	int last_digit = num % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
