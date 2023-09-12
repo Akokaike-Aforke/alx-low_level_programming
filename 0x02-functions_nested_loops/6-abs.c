fb5dac4dfc9576a62cc6def65850071708079870
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 12:51:39 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_abs - returns the absolute value
+ *
+ * Return: 1 if n is positive, 0 if n is 0, return -1 if n is negative
+ */
+int _abs(int)
+{
+	return abs(int);
+
+}
