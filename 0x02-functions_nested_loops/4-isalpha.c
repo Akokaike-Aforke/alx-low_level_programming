a245df525971904982affb091a3cb9153529b055
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 10:44:21 -0700

    letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_isalpha - check for lower case
+ *@c: the character to be checked
+ *
+ * Return: 1 if c is a letter, 0 if c is not a letter
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
