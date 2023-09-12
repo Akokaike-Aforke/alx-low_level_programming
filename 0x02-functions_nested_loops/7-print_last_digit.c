25b96782630c5a67be0d331467ec47e15af3f3ac
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 13:55:15 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *print_last_digit - return the absolute value
+ *@num: the value to check
+ *
+ * Return: the last digit of num
+ */
+int _abs(int num)
+{
+	return (num % 10);
+}
