03300a9fae96d28c95fef64953d8bf73031cfe27
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 16:18:36 -0700

    time

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *times_table - Prints times table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 10; j++)
+		{
+			_putchar((i * j)  '0');
+			_putchar('\n');
+		}
+	}
+}
