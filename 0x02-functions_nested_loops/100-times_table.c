1a2897d9d02fc1e171a7164465ec3ff949a40151
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 19:13:14 -0700

    mulitiplication

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *print_times_table - prints times table
+ *@n: the timestble to print
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n > 15 || n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf("%d, ", i * j);
+		}
+		_putchar('\n');
+	}
+}
