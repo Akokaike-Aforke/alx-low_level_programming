3bfbc93fa0bdca1186d0579b30a69f050331d158
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 18:59:25 -0700

    print numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,9 +9,17 @@
  */
 void print_to_98(int n)
 {
+	while (n >= 98)
+	{
+		if (n != 98)
+			printf("%d, ", n);
+		else
+			printf("%d", n);
+		n--;
+	}
 	while (n <= 98)
 	{
-		if(n != 98)
+		if (n != 98)
 			printf("%d, ", n);
 		else
 			printf("%d", n);
