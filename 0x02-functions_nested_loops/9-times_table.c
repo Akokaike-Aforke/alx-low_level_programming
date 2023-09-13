623b9f09e689c2dfd9bf163d99515a083a58b743
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 17:05:41 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,13 +15,14 @@ void times_table(void)
 		{
 			x = (i * j) / 10;
 			y = (i * j) % 10;
+
 			if(i * j >= 10)
 			{
 				_putchar(x + '0');
 				_putchar(y + '0');
 			}
 			else
-				_putchar(x);
+				_putchar(x + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
