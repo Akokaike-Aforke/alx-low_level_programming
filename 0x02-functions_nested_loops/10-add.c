0be2e4f1f4cf171a68da10b00ceca322e7b7c5fc
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-12 18:39:23 -0700

    add 2 numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,8 @@
 #include "main.h"
 /**
  *add - adds 2 numbers
- *@num1, @num2: the numbers to add
+ *@num1: first number to add
+ *@num2: second number to add
  *
  * Return: the sum of the 2 numbers
  */
