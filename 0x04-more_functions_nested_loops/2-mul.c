2a12acb65e5bb47a9598d2aad4b6f42f664fee5f
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-14 19:07:34 +0100

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiply 2 numbers
+ * @a: one number to be multiplied
+ * @b: second number to be multiplied
+ *
+ * Return: the a multiplied by b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
