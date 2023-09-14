b503d306286f11cb20766c25c47991ec13a9079d
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-14 18:58:41 +0100

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,7 +4,7 @@
  * _isdigit - Checks if a character is a digit or not
  * @c: the character to be checked
  *
- * Return 1 if c is a digit or 0 if not
+ * Return: 1 if c is a digit or 0 if not
  */
 
 int _isdigit(int c)
