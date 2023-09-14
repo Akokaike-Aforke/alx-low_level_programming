f75c99cb06e7d46af4cc3db581136ecfdeddd87b
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-14 18:53:25 +0100

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+ * _isdigit - Checks if a character is a digit or not
+ * @c: the character to be checked
+ *
+ * Return 1 if c is a digit or 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if(sdigit(c))
+		return (1);
+	return (0);
+}
