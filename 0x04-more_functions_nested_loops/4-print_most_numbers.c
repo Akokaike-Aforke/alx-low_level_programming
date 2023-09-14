17c80a6d23e6f54e1ce455fecc928b6ed4b3691a
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-14 13:02:14 -0700

    printing most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints 1-digit numbers except 2 and 4
+ * print_most_nubers - prints 1-digit numbers except 2 and 4
  *
  * Return: void
  */
 void print_most_numbers(void)
 {
 	char i;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != '2' || i != '4')
+		if (i != '2' && i != '4')
 			_putchar(i);
 	}
 	_putchar('\n');
