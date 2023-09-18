b85e949c16b8823b6da31ccfe49ccb1c0ce3f60e
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-09-18 20:26:21 +0100

    n elements of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - prints n elements of an array
+ * @a: pointer
+ * @n: number of elements of the array to be printed
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; x < n; i++)
+		if (i != n - 1)
+		printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+printf("\n");
+}
+}
