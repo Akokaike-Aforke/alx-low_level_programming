fa616f9192d0000a5e3e28ed32cb0cd00a1d272f
Akokaike-Aforke <lucykenneth54@gmail.com>
2023-10-30 21:08:29 +0100

    1-create

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,32 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
-  * append_text_to_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
-int append_text_to_file(const char *filename, char *text_content)
+ * create_file -creates an array of chars, and initializes
+ *
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(fd);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
